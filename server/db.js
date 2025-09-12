6b56e3035c175b153273e0908fd5d13bc0701419
NEXAD <45901794+MajidSoltaninezhad@users.noreply.github.com>
2025-09-12 10:47:47 +0800

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,11 +2,11 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL, // ← اینجا URL اکسترنال رو استفاده می‌کنیم
+  ssl: {
+    rejectUnauthorized: false // لازمه برای اکثر سرویس‌های ابری
+  }
 });
 
 module.exports = pool;
+
