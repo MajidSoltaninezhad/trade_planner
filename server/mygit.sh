bd2695e43ea916e8acecfe0c38c0c2ed8ea764aa
MajidSoltaninezhad <nezhad.majid@gmail.com>
2025-09-14 18:10:35 +0800

    ko

--- server/mygit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# اگر پیامی داده نشده بود، پیش‌فرض "Update"
-MSG=${1:-"Update"}
-
-git add .
-git commit -m "$MSG"
-git push origin main
--- trade_planner.session.sql
+++ trade_planner.session.sql
@@ -1,6 +1,7 @@
 
-DELETE FROM user_req;
-DELETE FROM user_calc;
+SELECT * FROM user_calc;
+-- DELETE FROM user_req;
+-- DELETE FROM user_calc;
 -- DELETE FROM user_req WHERE id = 8;
 
 
